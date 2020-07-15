ea48fc2de0adc6816ab4200861750e976a88a2c5
ldelbel <lucdelbel@gmail.com>
2020-07-15 09:53:42 -0300

    Add block_status_check and consec? methods do SmallChecks Class and  list_end_error and list_empty_lines_error methods to ErrorListing Class

--- lib/support.rb
+++ lib/support.rb
@@ -1,8 +1,15 @@
 require_relative './read.rb'
 
+class String
+  def blank?
+    self.strip.empty?
+  end
+end
+
 class Control
   
-  attr_accessor :reserved_words, :reserved_words_count, :error_list, :identation_value, :end_count
+  attr_accessor :reserved_words_count, :error_list, :identation_value, :end_count
+  attr_reader :reserved_words 
   
   def initialize
     @reserved_words = ["begin","\\case","class","def","do","if","module", "unless"]
@@ -23,34 +30,64 @@ class Control
       end
     end
   end
+
+  def reset
+    @error_list = []
+    @reserved_words_count = 0
+    @end_count = 0
+    @identation_value = 0
+  end
 end
 
 class ErrorListing
-  
+  attr_accessor :list
   def initialize
-    @error_list = []
+    @list = []
   end
   
   def list_ident_error(line_n, expec_ident)
-    @error_list << { line: line_n, error: "Identation error detected. Expected #{expec_ident} whitespaces."}
+    @list << { line: line_n, error: "Identation error detected. Expected #{expec_ident} whitespaces."}
   end
+
+  def list_trail_error(line_n)
+    @list << {line: line_n+1, error: 'C: Trailing whitespace detected.' } 
+  end
+
+  def list_end_error(status)
+    case status
+    when 1  then @list << {error: 'syntax error, unexpected end-of-input, expecting end' } 
+    when 0 then @list << 'JUST FOR TESTING' # REMOVE THIS ---------------------------------------------
+    when -1 then @list << {error: 'syntax error, unexpected end, expecting end-of-input' } 
+    end
+  end
+
+  def list_empty_lines_error(line_n, index)
+    @list << { line: line_n[index+1], error: 'Extra empty lines detected.'}
+  end
+  
 end
 
 class SmallChecks
   
-  def initialize
-    
-  end
-
-  def test_end(line_c)
+  def check_end(line_c)
     line_c.strip == 'end'
   end
   
-  def test_ident(line_c, expec_ident)
+  def check_ident(line_c, expec_ident)
     line_c.index(/[^ ]/).eql?(expec_ident)
   end
   
-  def test_ident_end(line_c, expec_ident)
+  def check_ident_end(line_c, expec_ident)
     line_c.index(/[^ ]/).eql?(expec_ident == 0 ? 0 : expec_ident - 2)
   end
+
+  def block_status_check(control_instance)
+    status = control_instance.reserved_words_count <=> control_instance.end_count
+  end
+
+  def consec?(line_n, index)
+    (line_n[index+1] - line_n[index]).eql?(1)
+  end
+
 end
+
